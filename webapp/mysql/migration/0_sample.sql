c6b4848864e0ab6b61ac736a9535b5f817774cc0
otoshigo <otatsuno27@gmail.com>
2025-11-16 07:36:49 +0900

    feat: 検索用インデックスを追加

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -2,3 +2,6 @@
 
 -- MySQL 5.7 互換のため IF NOT EXISTS は付けずに作成
 CREATE INDEX idx_orders_user_created_id ON orders(user_id, created_at, order_id);
+
+-- 検索用インデックス
+CREATE INDEX idx_products_name ON products(name);
